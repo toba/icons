ced12b85d992dfa9a69321b69a281e0952dea4a4
Jason Abbott <jasonabbott@Jason-Abbot-MBP15tb.local>
2019-03-05 09:31:06 -0700

    update main logo to colorless version

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
 export const icons: { [key: string]: string } = {
    ArrowBack: 'arrow-thick-circle-left', // Arrow Circle Left
    ArrowForward: 'arrow-thick-circle-right', // Arrow Circle Right
-   Logo: 'Logo Colored',
+   Logo: 'Toba Logo',
    NavMenuHorizontal: 'navigation-menu-horizontal',
    NavMenuVertical: 'navigation-menu-vertical',
    Settings: 'ic_settings_48px'
