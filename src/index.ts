1f006a16f28a0d9ef98bb7d62db1dc8fb328ec31
Jason Abbott <dev@trailimage.com>
2019-03-05 19:25:00 -0700

    add third party brands

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@
  * Map intended component name to SVG file name (without extension).
  */
 export const icons: { [key: string]: string } = {
+   Amazon: 'Amazon',
    ArrowBack: 'arrow-thick-circle-left', // Arrow Circle Left
    ArrowForward: 'arrow-thick-circle-right', // Arrow Circle Right
+   Dropbox: 'Dropbox',
+   Facebook: 'Facebook',
+   GitHub: 'Github',
+   Google: 'Google',
    Logo: 'Toba Logo',
    NavMenuHorizontal: 'navigation-menu-horizontal',
    NavMenuVertical: 'navigation-menu-vertical',
-   Settings: 'ic_settings_48px'
+   Settings: 'ic_settings_48px',
+   Yahoo: 'Yahoo'
 };
